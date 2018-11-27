e06260084207821d6c69749633103e72adf0fcc0
dendi <dendijoil@gmail.com>
2018-11-27 10:37:10 +0700

    update

--- app/src/main/java/com/example/jol/testing/SingleMapsActivity.java
+++ app/src/main/java/com/example/jol/testing/SingleMapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.jol.testing;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
@@ -10,6 +11,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
@@ -18,21 +21,35 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -48,7 +65,7 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
     private TextView xText, yText, zText, tvDistance;
     private Sensor SensorKu;
     private SensorManager SM;
-    private Button btStart;
+    private Button btStart, btSave, btRefresh;
     private String nextState = "start", X, Y, Z;
     Chronometer mChronometer;
     int timerCount = 1;
@@ -61,6 +78,7 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
     private BottomSheetBehavior bottomSheetBehavior;
     private Polyline gpsTrack;
     private List<LatLng> points = new ArrayList<>();
+    private Marker startMarker = null, stopMarker = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,9 +98,12 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
         tvDistance = findViewById(R.id.txtDistance);
 
         tvDistance.setVisibility(View.GONE);
+        btRefresh.setVisibility(View.GONE);
 
         // menghubungkan view button dengan controllernya
         btStart = findViewById(R.id.btnStart);
+        btSave = findViewById(R.id.btnSave);
+        btRefresh = findViewById(R.id.btnRefresh);
         mChronometer = findViewById(R.id.chronometer);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -93,6 +114,7 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
         //action ketika button start diklik
         btStart.setOnClickListener(view -> {
             if (nextState.equals("start")) {
+                btRefresh.setVisibility(View.GONE);
                 btStart.setText("stop");
                 initLocation();
                 timerCount = 1;
@@ -103,8 +125,13 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
                 dataSensor = new ArrayList<>();
                 SM.registerListener(SingleMapsActivity.this, SensorKu, SensorManager.SENSOR_DELAY_NORMAL);
                 Toast.makeText(SingleMapsActivity.this, "Sensor Running..!", Toast.LENGTH_SHORT).show();
-
+                if (startMarker != null && stopMarker != null) {
+                    startMarker.remove();
+                    stopMarker.remove();
+                    gpsTrack.remove();
+                }
             } else if (nextState.equals("stop")) {
+                btRefresh.setVisibility(View.VISIBLE);
                 btStart.setText("start");
                 nextState = "start";
                 timerCount = 1;
@@ -115,8 +142,8 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
                 Toast.makeText(SingleMapsActivity.this, "Sensor Stopped...!", Toast.LENGTH_SHORT).show();
                 tvDistance.setText("Distance = " + String.valueOf(getDistancBetweenTwoPoints()) + " meter");
                 tvDistance.setVisibility(View.VISIBLE);
-                mMap.addMarker(new MarkerOptions().position(points.get(0)).title("Start Position"));
-                mMap.addMarker(new MarkerOptions().position(points.get(points.size() - 1)).title("Stop Position"));
+                startMarker = mMap.addMarker(new MarkerOptions().position(points.get(0)).title("Start Position"));
+                stopMarker = mMap.addMarker(new MarkerOptions().position(points.get(points.size() - 1)).title("Stop Position"));
             }
         });
 
@@ -127,6 +154,34 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
             int second = (int) (timeElapsed - hours * 3600000 - minutes * 60000) / 1000;
             System.out.println("second : " + (second + 1));
             timerCount = second + 1;
+        });
+
+        btRefresh.setOnClickListener(v -> {
+            mChronometer.setBase(SystemClock.elapsedRealtime());
+            timerCount = 1;
+            tvDistance.setVisibility(View.GONE);
+            xText.setText("");
+            yText.setText("");
+            zText.setText("");
+            if (startMarker != null && stopMarker != null) {
+                startMarker.remove();
+                stopMarker.remove();
+                gpsTrack.remove();
+            }
+        });
+
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //tombol save hanya bisa berfungsi ketika tombol stop sudah ditekan dan dataSensor tidak kosong
+                if (nextState.equals("start") && !dataSensor.isEmpty()) {
+//                    Toast.makeText(Home.this, "tes", Toast.LENGTH_SHORT).show();
+                    //kode savenya dibuat ke method lain aja, klo ditulis disini kepanjangan, gaenak diliat :v
+                    saveDatatoExcel();
+                } else {
+                    Toast.makeText(SingleMapsActivity.this, "Data Sensor kosong / sensor masih berjalan !", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
 
         // get the bottom sheet view
@@ -224,6 +279,98 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
         return distance[0];
     }
 
+    private void saveDatatoExcel() {
+        //Create Blank workbook atau file excel
+        final HSSFWorkbook workbook = new HSSFWorkbook();
+        //Create a blank sheet
+        HSSFSheet spreadsheet = workbook.createSheet(" Sensor Acc Data ");
+        //Create row object
+        HSSFRow headerRow;
+
+        String[] columns = {"Time", "X", "Y", "Z", "Latitude", "Longitude", "Speed(meter/second)"};
+        //Header
+        headerRow = spreadsheet.createRow(0);
+        for (int i = 0; i < columns.length; i++) {
+            Cell cell = headerRow.createCell(i);
+            cell.setCellValue(columns[i]);
+        }
+
+        // Create Other rows and cells with sensor data
+        int rowNum = 1;
+        for (ModelSensor data : dataSensor) {
+            HSSFRow row = spreadsheet.createRow(rowNum++);
+            row.createCell(0).setCellValue(data.getTime());
+            row.createCell(1).setCellValue(data.getX());
+            row.createCell(2).setCellValue(data.getY());
+            row.createCell(3).setCellValue(data.getZ());
+            row.createCell(4).setCellValue(data.getLatitude());
+            row.createCell(5).setCellValue(data.getLongitude());
+            row.createCell(6).setCellValue(data.getCurrSpeed());
+//            System.out.println(data.getX() + "," + data.getY() + "," + data.getZ());
+        }
+        showDialog(workbook);
+    }
+
+    private void saveFile(HSSFWorkbook workbook, String fileName) {
+        // Write the output to a file
+        FileOutputStream fileOut = null;
+        try {
+            String root = Environment.getExternalStorageDirectory().toString();
+            File myDir = new File(root + "/Sensor_Data");
+            myDir.mkdirs();
+            String fname = "";
+            fname = "SensorData_" + fileName + ".csv";
+            File file = new File(myDir, fname);
+            if (file.exists())
+                file.delete();
+            fileOut = new FileOutputStream(file);
+            workbook.write(fileOut);
+            fileOut.close();
+            // Closing the workbook
+            workbook.close();
+            // Tell the media scanner about the new file so that it is
+            // immediately available to the user.
+            MediaScannerConnection.scanFile(this,
+                    new String[]{file.toString()}, null,
+                    (path, uri) -> {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    });
+            Log.d("path file", "Sensor data has been saved to Data_Sensor.csv in " + myDir.getPath() + "!");
+            Toast.makeText(SingleMapsActivity.this, "Sensor data has been saved to Data_Sensor.xlsx in " + myDir.getPath() + "!", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException eF) {
+            Log.d("FileNotFound Exc", eF.getMessage());
+            eF.printStackTrace();
+        } catch (IOException e) {
+            Log.d("IO Exc", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void showDialog(HSSFWorkbook workbook) {
+        Dialog dialogOption = new Dialog(this);
+        dialogOption.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialogOption.setContentView(R.layout.dialog_input_filename);
+        dialogOption.setCancelable(true);
+        ViewGroup.LayoutParams params = dialogOption.getWindow().getAttributes();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        dialogOption.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+
+
+        EditText name = dialogOption.findViewById(R.id.etFileName);
+        Button btOk = dialogOption.findViewById(R.id.btOk);
+
+        btOk.setOnClickListener(v -> {
+            if (!name.getText().toString().equals("")) {
+                saveFile(workbook, name.getText().toString());
+                dialogOption.dismiss();
+            } else
+                Toast.makeText(this, "Please fill the File Name", Toast.LENGTH_SHORT).show();
+        });
+        dialogOption.show();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -286,6 +433,10 @@ public class SingleMapsActivity extends FragmentActivity implements OnMapReadyCa
             points = gpsTrack.getPoints();
             points.add(new LatLng(location.getLatitude(), location.getLongitude()));
             gpsTrack.setPoints(points);
+            CameraUpdate center = CameraUpdateFactory.newLatLng(points.get(points.size() - 1));
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(18);
+            mMap.moveCamera(center);
+            mMap.animateCamera(zoom);
             CLocation myLocation = new CLocation(location, this.useMetricUnits());
             this.updateSpeed(myLocation);
         }
